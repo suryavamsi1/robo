2016a2b1fd67a85acd3321605d0804e064cac208
Surya Vamsi <suryavamsisaridey@gmail.com>
2020-10-06 10:44:27 +0530

    Updates

---
+++ precache-manifest.3193052943066bcf3d03cb7ec09423e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff9444dab1010e31004c81bbaaa17be",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "c409aa202de24f25fa3d",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8174cec05f5a178680c6",
+    "url": "/robo/static/css/main.1f78abfa.chunk.css"
+  },
+  {
+    "revision": "c409aa202de24f25fa3d",
+    "url": "/robo/static/js/2.6ced7727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.6ced7727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8174cec05f5a178680c6",
+    "url": "/robo/static/js/main.b080ffac.chunk.js"
+  },
+  {
+    "revision": "cd01cbebd96a4c029298",
+    "url": "/robo/static/js/runtime-main.a78c4623.js"
+  },
+  {
+    "revision": "22e05b86a5bc56cf75b9ab7683fa4309",
+    "url": "/robo/static/media/Alien Robot.22e05b86.woff"
+  }
+]);
